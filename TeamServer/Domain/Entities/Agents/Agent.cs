ab7a8e874c08f0c0c7030c5072b401116a8dd710
WG37 <willg2070@gmail.com>
2025-11-17 15:17:14 +1100

    fix(): added attribute to hide Db key from API

--- TeamServer/Domain/Entities/Agents/Agent.cs
+++ TeamServer/Domain/Entities/Agents/Agent.cs
@@ -1,15 +1,26 @@
-﻿namespace TeamServer.Domain.Entities.Agents
+﻿using System.Text.Json.Serialization;
+
+namespace TeamServer.Domain.Entities.Agents
 {
     public class Agent
     {
+        [JsonIgnore]
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid UniqueId { get; set; }
         public AgentMetadata Metadata { get; set; }
+
+        public DateTime LastSeen { get; private set; }
 
         public Agent(Guid uniqueId, AgentMetadata metadata)
         {
             UniqueId = uniqueId;
             Metadata = metadata;
+            LastSeen = DateTime.UtcNow;
+        }
+
+        public void CheckIn()
+        {
+            LastSeen = DateTime.UtcNow;
         }
 
         protected Agent() { } // EF core
