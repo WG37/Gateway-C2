81fac586bd899581bdbfec1e5b790736da93e946
WG37 <willg2070@gmail.com>
2025-11-17 15:17:57 +1100

    feat(): migrations

--- TeamServer/Migrations/20251117024224_RemoveLastSeen.Designer.cs
+++ TeamServer/Migrations/20251117024224_RemoveLastSeen.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeamServer.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using TeamServer.Infrastructure.Data;
 namespace TeamServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251117024224_RemoveLastSeen")]
+    partial class RemoveLastSeen
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -27,9 +30,6 @@ namespace TeamServer.Migrations
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
-
-                    b.Property<DateTime>("LastSeen")
-                        .HasColumnType("datetime2");
 
                     b.Property<Guid>("UniqueId")
                         .HasColumnType("uniqueidentifier");
