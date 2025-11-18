18b91fab9694f04b564409019846974b45ae379d
WG37 <willg2070@gmail.com>
2025-11-18 16:18:23 +1100

    feat(): added data annotations

--- Agent/Domain/Models/Agents/AgentTask.cs
+++ Agent/Domain/Models/Agents/AgentTask.cs
@@ -1,10 +1,20 @@
-﻿namespace AgentClient.Domain.Models.Agents
+﻿using System.Runtime.Serialization;
+
+namespace AgentClient.Domain.Models.Agents
 {
+    [DataContract]
     public class AgentTask
     {
+        [DataMember(Name = "id")]
         public Guid Id { get; set; }
+
+        [DataMember(Name = "command")]
         public string Command { get; set; }
+
+        [DataMember(Name = "arguments")]
         public string[] Arguments { get; set; }
+
+        [DataMember(Name = "file")]
         public byte[] File { get; set; }
     }
 }
