cac3e9725fd7b623d23a36a7f00bbb5c233aee01
WG37 <willg2070@gmail.com>
2025-11-19 13:55:53 +1100

    feat(): added base class AgentCommand

---
+++ Agent/Application/Commands/AgentCommand.cs
@@ -0,0 +1,11 @@
+﻿using AgentClient.Domain.Models.Agents;
+
+namespace AgentClient.Application.Commands
+{
+    public abstract class AgentCommand
+    {
+        public abstract string Name { get; }
+
+        public abstract string Execute(AgentTask task);
+    }
+}
