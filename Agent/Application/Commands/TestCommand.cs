b732203f9ed60e3da6c08bce9caaa9b7d40f370f
WG37 <willg2070@gmail.com>
2025-11-20 15:13:31 +1100

    feat(): added testCommand

---
+++ Agent/Application/Commands/TestCommand.cs
@@ -0,0 +1,15 @@
+﻿
+using AgentClient.Domain.Models.Agents;
+
+namespace AgentClient.Application.Commands
+{
+    public class TestCommand : AgentCommand
+    {
+        public override string Name => "TestCommand";
+
+        public override string Execute(AgentTask task)
+        {
+            return "Hello from TestCommand";
+        }
+    }
+}
