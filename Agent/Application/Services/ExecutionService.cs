17f39490eef9e8497333fd62b109883f7dd08b3b
WG37 <willg2070@gmail.com>
2025-12-03 15:34:53 +1100

    feat(): added service for executes

---
+++ Agent/Application/Services/ExecutionService.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace AgentClient.Application.Services
+{
+    public static class ExecutionService
+    {
+        public static string ExecuteCommand(string fileName, string args)
+        {
+            var stdout = "";
+
+            var pStartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = args,
+                WorkingDirectory = Directory.GetCurrentDirectory(),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(pStartInfo);
+
+            using (process.StandardOutput)
+            {
+                stdout += process.StandardOutput.ReadToEnd();
+            }
+
+            using (process.StandardError)
+            {
+                stdout += process.StandardError.ReadToEnd();
+            }
+
+            return stdout;
+        }
+    }
+}
