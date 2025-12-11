9403a69c880a029c8d8e7840c09b255d04cf0122
WG37 <willg2070@gmail.com>
2025-12-11 13:04:12 +1100

    feat(kernel32.cs): add CloseHandle()

---
+++ Agent/Infrastructure/Native/Kernel32.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AgentClient.Infrastructure.Native
+{
+    public class Kernel32
+    {
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+    }
+}
