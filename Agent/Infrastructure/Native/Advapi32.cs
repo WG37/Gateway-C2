8125b3d03bd3beb92d71e2a0592e0cba5a4b4c08
WG37 <willg2070@gmail.com>
2025-12-12 13:23:52 +1100

    feat(Advapi): add enums for desired/tokenAccess + security attributes

--- Agent/Infrastructure/Native/Advapi32.cs
+++ Agent/Infrastructure/Native/Advapi32.cs
@@ -4,6 +4,48 @@ namespace AgentClient.Infrastructure.Native
 {
     public class Advapi32
     {
+        public enum DesiredAccess
+        {
+            STANDARD_RIGHTS_REQUIRED = 0x000F0000,
+            STANDARD_RIGHTS_READ = 0x00020000,
+            TOKEN_ASSIGN_PRIMARY = 0x0001,
+            TOKEN_DUPLICATE = 0x0002,
+            TOKEN_IMPERSONATE = 0x0004,
+            TOKEN_QUERY = 0x0008,
+            TOKEN_QUERY_SOURCE = 0x0010,
+            TOKEN_ADJUST_PRIVILEGES = 0x0020,
+            TOKEN_ADJUST_GROUPS = 0x0040,
+            TOKEN_ADJUST_DEFAULT = 0x0080,
+            TOKEN_ADJUST_SESSIONID = 0x0100,
+            TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY),
+
+            TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
+        TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE |
+        TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT |
+        TOKEN_ADJUST_SESSIONID)
+        };
+        public enum TokenAccess
+        {
+            TOKEN_ASSIGN_PRIMARY = 0x0001,
+            TOKEN_DUPLICATE = 0x0002,
+            TOKEN_IMPERSONATE = 0x0004,
+            TOKEN_QUERY = 0x0008,
+            TOKEN_QUERY_SOURCE = 0x0010,
+            TOKEN_ADJUST_PRIVILEGES = 0x0020,
+            TOKEN_ADJUST_GROUPS = 0x0040,
+            TOKEN_ADJUST_DEFAULT = 0x0080,
+            TOKEN_ADJUST_SESSIONID = 0x0100,
+            TOKEN_ALL_ACCESS_P = 0x000F00FF,
+            TOKEN_ALL_ACCESS = 0x000F01FF,
+            TOKEN_READ = 0x00020008,
+            TOKEN_WRITE = 0x000200E0,
+            TOKEN_EXECUTE = 0x00020000
+        }
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        };
         public enum LogonUserProvider
         {
             LOGON32_PROVIDER_DEFAULT = 0,
@@ -12,7 +54,6 @@ namespace AgentClient.Infrastructure.Native
             LOGON32_PROVIDER_WINNT50 = 3,
             LOGON32_PROVIDER_VIRTUAL = 4
         };
-
         public enum LogonUserType
         {
             LOGON32_LOGON_INTERACTIVE = 2,
@@ -24,6 +65,22 @@ namespace AgentClient.Infrastructure.Native
             LOGON32_LOGON_CREDENTIAL = 9,
             LOGON32_LOGON_NEW_CREDENTIALS = 10
         };
+        public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous,
+            SecurityIdentification,
+            SecurityImpersonation,
+            SecurityDelegation
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
 
         [DllImport("Advapi32.dll",
             SetLastError = true, CharSet = CharSet.Ansi)]
@@ -36,10 +93,31 @@ namespace AgentClient.Infrastructure.Native
             out IntPtr phToken
             );
 
+
+        [DllImport("Advapi32.dll", SetLastError = true)]
+        public static extern bool OpenProcessToken(
+            IntPtr pHandle,
+            DesiredAccess desiredAcess,
+            out IntPtr hToken
+            );
+
+
+        [DllImport("Advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            TokenAccess dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpTokenAttributes,
+            SECURITY_IMPERSONATION_LEVEL impersonationLevel,
+            TOKEN_TYPE tokenType,
+            out IntPtr phNewToken
+            );
+
+
         [DllImport("Advapi32.dll", SetLastError = true)]
         public static extern bool ImpersonateLoggedOnUser(
             IntPtr hToken
             );
+
 
         [DllImport("Advapi32.dll", SetLastError = true)]
         public static extern bool RevertToSelf();
