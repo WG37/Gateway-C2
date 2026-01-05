1def48323b93f4eb5ddf38fd65c147745a1140a5
WG37 <willg2070@gmail.com>
2026-01-05 12:28:50 +1100

    feat(): add DEV-ONLY key for testing

---
+++ Agent/Infrastructure/Utilities/KeyStorage/KeyStore.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AgentClient.Infrastructure.Utilities.KeyStorage
+{
+    public static class KeyStore
+    {
+        // DEV ONLY
+        public static readonly byte[] devKey = Convert.FromBase64String("QdZFH9uRqpb3HQ+T4KGd0Qx35f2ZwAHpEQcOJzCN9Go=");
+    }
+}
