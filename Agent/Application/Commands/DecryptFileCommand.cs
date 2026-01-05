9969085ea5f83b5f3f7448496a5f919a4d3d4d75
WG37 <willg2070@gmail.com>
2026-01-05 12:28:17 +1100

    feat(): add encrypt/decrypt commands

---
+++ Agent/Application/Commands/DecryptFileCommand.cs
@@ -0,0 +1,37 @@
+﻿using AgentClient.Domain.Models.Agents;
+using AgentClient.Infrastructure.Cryptography.FileDecryption;
+using AgentClient.Infrastructure.Utilities.KeyStorage;
+
+namespace AgentClient.Application.Commands
+{
+    public class DecryptFileCommand : AgentCommand
+    {
+        public override string Name => "decrypt-aes";
+
+        public override string Execute(AgentTask task)
+        {
+            string path;
+
+            if (task.Arguments == null || task.Arguments.Length == 0)
+            {
+                return "Arguments cannot be null or empty";
+            }
+            else
+            {
+                path = task.Arguments[0];
+            }
+
+            try
+            {
+                var decrypt = new FileDecryptor(KeyStore.devKey);
+                decrypt.DecryptFile(path);
+            }
+            catch (Exception ex)
+            {
+                return $"Decryption failed: {ex.Message}";
+            }
+
+            return "Decryption successful";
+        }
+    }
+}
---
+++ Agent/Application/Commands/EncryptFileCommand.cs
@@ -0,0 +1,38 @@
+﻿using AgentClient.Infrastructure.Utilities.KeyGen;
+using AgentClient.Infrastructure.Utilities.KeyStorage;
+using AgentClient.Domain.Models.Agents;
+using AgentClient.Infrastructure.Cryptography.FileEncryption;
+
+namespace AgentClient.Application.Commands
+{
+    public class EncryptFileCommand : AgentCommand
+    {
+        public override string Name => "encrypt-aes";
+
+        public override string Execute(AgentTask task)
+        {
+            string path;
+
+            if (task.Arguments == null || task.Arguments.Length == 0)
+            {
+                return "Arguments cannot be null or empty";
+            }
+            else
+            {
+                path = task.Arguments[0];                
+            }
+
+            try
+            {
+                var encrypt = new FileEncryptor(KeyStore.devKey);
+                encrypt.EncryptFile(path);
+            }
+            catch (Exception ex)
+            {
+                return $"Encryption failed: {ex.Message}";
+            }
+
+            return "Encryption successful";
+        }
+    }
+}
