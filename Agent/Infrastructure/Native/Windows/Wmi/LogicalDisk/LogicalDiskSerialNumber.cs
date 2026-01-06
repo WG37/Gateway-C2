704418ff2e7aababa0d7e4a3c8778b7a272d7478
WG37 <willg2070@gmail.com>
2026-01-06 16:10:46 +1100

    feat(): add processor Id and Disk Serial Number

---
+++ Agent/Infrastructure/Native/Windows/Wmi/LogicalDisk/LogicalDiskSerialNumber.cs
@@ -0,0 +1,21 @@
+﻿using System.Management;
+
+namespace AgentClient.Infrastructure.Native.Windows.Wmi.LogicalDisk
+{
+    public class LogicalDiskSerialNumber
+    {
+        public static string GetLogicalDiskSerialNumber()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT VolumeSerialNumber FROM Win32_LogicalDisk"))
+            {
+                foreach (var obj in searcher.Get())
+                {
+                    var serial = obj["VolumeSerialNumber"]?.ToString() ?? "";
+
+                    return serial;
+                }
+            }
+            return "";
+        }
+    }
+}
---
+++ Agent/Infrastructure/Native/Windows/Wmi/Processor/ProcessorId.cs
@@ -0,0 +1,21 @@
+﻿using System.Management;
+
+namespace AgentClient.Infrastructure.Native.Windows.Wmi.Processor
+{
+    public class ProcessorId
+    {
+        public static string GetProcessorId()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT ProcessorId FROM Win32_Processor"))
+            {
+                foreach (var obj in searcher.Get())
+                {
+                    var id = obj["ProcessorId"]?.ToString() ?? "";
+
+                    return id;
+                }
+            }
+            return "";
+        }
+    }
+}
