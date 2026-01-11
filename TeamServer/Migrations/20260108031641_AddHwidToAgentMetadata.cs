44a42fbe0dddc433b6cea8995a38a5c54c147428
WG37 <willg2070@gmail.com>
2026-01-11 13:20:22 +1100

    add migrations

---
+++ TeamServer/Migrations/20260108031641_AddHwidToAgentMetadata.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHwidToAgentMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Metadata_Hwid",
+                table: "Agents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Metadata_Hwid",
+                table: "Agents");
+        }
+    }
+}
